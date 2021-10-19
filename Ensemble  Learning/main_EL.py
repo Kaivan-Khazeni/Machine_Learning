c78ad5ccc75db949d72a84e1329283d8ce95f0c3
kkhazeni7 <u1135484@utah.edu>
2021-10-19 17:28:22 -0600

    working to fix ID3, not creating different stumps which is affecting the lack of variety in my predictions

--- Ensemble  Learning/main_EL.py
+++ Ensemble  Learning/main_EL.py
@@ -26,7 +26,7 @@ def find_total_entropy(max_labels,variant,S):
     #print(S)
     p = S.loc[S['y'] == 1, 'D_t'].sum()
     _p = S.loc[S['y'] == -1, 'D_t'].sum()
-    error = -p * np.log(p) - _p * np.log(_p)
+    error = -1*p * np.log(p) - _p * np.log(_p)
 
     return error
   elif variant == 2:
@@ -58,23 +58,39 @@ def find_best_split(max_labels, variant, attr, S, total_ent):
     # Need to check the
 
     list_of_IG = {}
+    all_attr_entropy = {}
     for A1 in attr:
+
+        all_attr_entropy[A1] = 0
         curr_attr = A1
 
         curr_df = S[[curr_attr, 'y','D_t']]
-        #print(curr_df)
 
+        #print(curr_df)
+        attr_entropy = find_total_entropy(max_labels,variant,curr_df)
         len_A1_df = len(curr_df[curr_attr])
         found_sum_expected_entropy = 0
         all_attr_values = curr_df[curr_attr].unique()
+        prob_ = {}
+        list_of_IG = {val: 0 for val in curr_df[curr_attr].unique()}
         for A2 in all_attr_values:
             attribute_specific = curr_df[curr_df[curr_attr] == A2]
-            found_expected_entropy = 0
-            entropy = find_total_entropy(max_labels, variant, attribute_specific)
-            found_sum_expected_entropy += (entropy * (len(attribute_specific) / len_A1_df))
-        list_of_IG[A1] = total_ent - found_sum_expected_entropy
-        #print(list_of_IG)
-    return list_of_IG
+            prob_[A2] = {}
+            prob_[A2]["pos"] = sum(attribute_specific[attribute_specific.y == 1]['D_t'])/sum(attribute_specific['D_t'])
+            prob_[A2]["neg"] = sum(attribute_specific[attribute_specific.y == -1]['D_t'])/sum(attribute_specific['D_t'])
+
+        for k,v in prob_.items():
+            for val,prob in v.items():
+                list_of_IG[k] += -1 * prob * np.log(prob)
+        total_entropy_attribute = {val: 0 for val in curr_df[curr_attr].unique()}
+
+        for val in curr_df[curr_attr].unique():
+            prob_val = len(curr_df[curr_df[curr_attr] == val])/len(curr_df)
+            total_entropy_attribute[val] = prob_val * list_of_IG[val]
+
+        all_attr_entropy[A1] = total_ent - sum(total_entropy_attribute.values())
+
+    return all_attr_entropy
 
 def check_for_numerical(df_S):
     import statistics
@@ -94,7 +110,7 @@ def ID3(max_labels, max_depth, variant, S, A, tree=None):
     max_labels = len(S['y'].unique())
     labels_value = S['y'].unique()
     Class = 'y'
-    # Step 1: check if all examplesa have the same label
+    # Step 1: check if all examples have the same label
     # keep counter to make sure iterations do not exceed the max depth
     if max_depth >= 0:
         # Finding root based off of gain
@@ -104,18 +120,25 @@ def ID3(max_labels, max_depth, variant, S, A, tree=None):
         table_attributes = S[S.columns[~S.columns.isin(['y','D_t','Weighted_y','pred','D_t+1'])]].columns
         best_A = find_best_split(max_labels, variant, table_attributes, S, total_entropy)
         A = sorted(best_A.items(), key=lambda x: x[1], reverse=True)[0][0]
+
         tree = {}
         tree[A] = {}
-        if max_depth == 0:
-            tree[A] = S['y'].mode()[0]
-            return tree
+        max_depth -= 1
         try:
             values_of_A = S[A].unique()
         except:
-            print(S)
+            print('error')
+
         for A_val in values_of_A:
             Sv = S.loc[S[A] == A_val, S.columns != A]
             common, counts = np.unique(Sv['y'], return_counts=True)
+            p = S.loc[S['y'] == 1, 'D_t'].sum()
+            _p = S.loc[S['y'] == -1, 'D_t'].sum()
+            if p > _p:
+                tree[A][A_val] = 1
+            else:
+                tree[A][A_val] = -1
+                """
             if len(Sv.columns) == 1 or len(counts) == 1:
                 tree[A][A_val] = common[0]
             else:
@@ -125,69 +148,9 @@ def ID3(max_labels, max_depth, variant, S, A, tree=None):
                     tree[A][A_val] = 1
                 else:
                     tree[A][A_val] = -1
+                    """
         return total_entropy,tree
 
-def get_error_of_trees(df_A,df_test_A):
-    # TRAINING ERROR
-    # DEPTH 1 -> 6
-    # RECORD ERROR
-    list_of_variants = ["Entropy", "MajorityError", "Gini Index"]
-    error_dict_per_variant = {}
-
-    testing_error = {}
-    for b in range(1, 4):
-        error_dict_per_variant[list_of_variants[b - 1]] = {}
-        total = []
-        total_test = []
-        testing_error[list_of_variants[b - 1]] = {}
-        for i in range(1, len(df_A.columns)):
-
-            error_counter = 0
-            error_counter_test = 0
-
-            train_tree = ID3(-1 * float("inf"), i, b, df_A, "none", tree=None)
-            train_tree_path = list(dict_path(train_tree, path=None))
-            for j in range(len(train_tree_path)):
-                df_filter = df_A
-                df_filter_test = df_test_A
-                curr_path = train_tree_path[j]
-                for a in range(0, len(curr_path[0])):
-                    if (a % 2) == 0:
-                        df_filter = df_filter[df_filter[curr_path[0][a]] == curr_path[0][a + 1]]
-                        df_filter_test = df_filter_test[df_filter_test[curr_path[0][a]] == curr_path[0][a + 1]]
-
-                length = len(df_filter)
-                length_test = len(df_filter_test)
-
-                count_of_accuracies = len(df_filter[df_filter['y'] == curr_path[1]])
-                count_of_accuracies_test = len(
-                    df_filter_test[df_filter_test['y'] == curr_path[1]])
-
-                count_of_inaccuracies = length - count_of_accuracies
-                count_of_inaccuracies_test = length_test - count_of_accuracies_test
-
-                if length == 0:
-                    error_counter += 0
-                else:
-                    error_counter += count_of_inaccuracies / length
-                if length_test == 0:
-                    error_counter_test += 0
-                else:
-                    error_counter_test += count_of_inaccuracies_test / length_test
-
-            total.append(error_counter / len(train_tree_path))
-            total_test.append(error_counter_test/len(train_tree_path))
-
-        error_dict_per_variant[list_of_variants[b - 1]] = np.mean(total)
-        testing_error[list_of_variants[b - 1]] = np.mean(total_test)
-
-    Errors = {'Train Average Error': [error_dict_per_variant['Entropy'], error_dict_per_variant['MajorityError'],
-                                      error_dict_per_variant['Gini Index']],
-              'Test Average Error': [testing_error['Entropy'], testing_error['MajorityError'],
-                                     testing_error['Gini Index']]}
-
-    error_df = pd.DataFrame(Errors, index=['Entropy', 'MajorityError', 'Gini Index'])
-    return error_df
 
 def adaboost(S,S_test,t):
     dict_ = {}
@@ -195,13 +158,13 @@ def adaboost(S,S_test,t):
     errors_per_t = []
     training_error = []
     errors_ = {}
-
     errors_["train"] = []
     errors_["test"] = []
 
-    for i in range(0,t):
-        dict_[i] = {}
+    for i in range(1,t):
+        dict_[i-1] = {}
         entropy , train_tree = ID3(-1 * float("inf"), 1, 1, S, "none", tree=None)
+        print(train_tree)
         paths = list(dict_path(train_tree, path=None))
         for path in paths:
             attr = path[0][0]
@@ -209,50 +172,44 @@ def adaboost(S,S_test,t):
             label = path[1]
             S.loc[S[attr] == value , 'pred'] = label
             S_test.loc[S_test[attr] == value, 'pred'] = label
-
         error_train = S.loc[S['y'] != S['pred'], 'D_t'].sum()
         error_test = S_test.loc[S_test['y'] != S_test['pred'], 'D_t'].sum()
-        errors_['train'].append(error_train / np.sum(S['D_t']))
-        errors_['test'].append(error_test/ np.sum(S_test['D_t']))
-
-
-        errors_per_t.append(error_train)
 
         vote = 0.5*np.log((1-error_train)/error_train)
-        dict_[i]['vote'] = vote
-        dict_[i]['pred'] = S['pred'].values
+        dict_[i-1]['vote'] = vote
+        dict_[i-1]['pred'] = S['pred'].values
+        dict_[i-1]['pred_test'] = S_test['pred'].values
 
 
-        S['D_t+1'] = S['D_t'] * np.power(2.718281 , (-1 * round(vote,3) * S['y'] * S['pred']))
-        S['D_t+1'] = S['D_t+1']/sum(S['D_t'])
+        S['D_t+1'] = (S['D_t']/sum(S['D_t'])) * np.power(2.718281 , (-1 * round(vote,3) * S['y'] * S['pred']))
+        #S['D_t+1'] = S['D_t+1']/sum(S['D_t'])
         S['D_t'] = S['D_t+1']
         #print(i)
     #here I will do error after we get the final H
-    final_pred = []
-    #isSame = True
-    #training error
-    #err = []
-    #for t_ in range(t):
-    # training_error = []
-    count_train = 0
-    count_test = 0
-    for i in range(len(S)):
-        h_i = []
-        for T in range(0,t):
-            vote_A = dict_[T]['vote']
-            h_pred = dict_[T]['pred'][i]
-            h_i.append(vote_A * h_pred)
-        final_pred.append(np.sign(sum(h_i)))
-        if np.sign(sum(h_i)) != S['y'][i]:
-            count_train += 1
-        if np.sign(sum(h_i)) != S_test['y'][i]:
-            count_test += 1
-        #training_error.append(np.power(2.71828, S['D_t'][i] * final_pred[i] * S['y'][i]))
-    #err.append(np.sum(training_error))
-    #print(err)
-    return errors_, count_train,count_test
+        final_pred_train = []
+        final_pred_test = []
+        count_train = 0
+        count_test = 0
+        for j in range(len(S)):
+            h_i = []
+            h_i_test = []
+            for T in range(0,i):
+                vote_A = dict_[T]['vote']
+                h_pred = dict_[T]['pred'][j]
+                h_pred_test = dict_[T]['pred_test'][j]
+                h_i.append(vote_A * h_pred)
+                h_i_test.append(vote_A * h_pred_test)
+            final_pred_train.append(np.sign(sum(h_i)))
+            final_pred_test.append(np.sign(sum(h_i_test)))
 
-
+            if final_pred_train[j] != S['y'].loc[j]:
+                count_train += 1
+            if final_pred_test[j] != S_test['y'].loc[j]:
+                count_test += 1
+        #print(final_pred_train)
+        errors_['train'].append(count_train)
+        errors_['test'].append(count_test)
+    return errors_['train'], errors_['test']
 
 if __name__ == '__main__':
     bank_attributes = ["age", "job", "marital", "education", "default", "balance", "housing", "loan", "contact", "day",
@@ -291,21 +248,12 @@ if __name__ == '__main__':
     errors_per_t_test = []
     axis_y = []
 
+    train_error,test_error = adaboost(bank_df_train, bank_df_test, 10)
+    print(train_error)
+    print(test_error)
 
-    for i in range(1,10):
-
-        e, e1 = adaboost(bank_df_train,bank_df_test,i)
-
-        e = (e/5000)
-        e1 = (e1/5000)
-
-
-        errors_per_t_train.append(1 / np.power(2.71828 , e * i))
-        errors_per_t_test.append(1 / np.power(2.71828, e1* i))
-        axis_y.append(i)
 
     #print(errors_per_t)
-
 
     data_a = errors_per_t_train
     data_b = errors_per_t_test
--- Linear Regression/main_LR.py
+++ Linear Regression/main_LR.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def cost_function(X,y, theta):
     m = len(X)
     pred = X.dot(theta.T)
-    cost = (1/2*m) * np.sum(np.square(pred - y))
+    cost = (1/2*m) * np.sum(np.square(pred-y))
     return cost
 
 def batch_gradient_descent(S , theta, r, iter):
@@ -54,66 +54,33 @@ def batch_gradient_descent(S , theta, r, iter):
     return sum_by_m
     """
 def stochastic_gradient_descent(X, theta, r, iterations):
-    """
-    w = []
+
     m = len(X)
-    y = X.output
-    X = X[['Cement', 'Slag', 'Fly ash', 'Water', 'SP', 'Coarse Aggr', 'Fine Aggr']]
-    w.append(np.zeros(7))
     cost_arr = np.zeros(iterations)
 
+    y = X.output
+    X = X[['Cement', 'Slag', 'Fly ash', 'Water', 'SP', 'Coarse Aggr', 'Fine Aggr']]
+    theta_arr = np.zeros((iterations,7))
+
+    converged = False
     for iter in range(iterations):
-        w.append(np.zeros(7))
         cost = 0
         for i in range(m):
-            rand = np.random.randint(0,m)
-            y_i = y[rand]
-            x_i = X.iloc[rand].values
-
-            pred = x_i.dot(w[iter].T)
-            for t in range(7):
-                w[iter + 1][t] = w[iter][t] + r * (y_i - pred)*x_i[t]
-            cost += cost_function(x_i, y_i, w[iter])
+            x_i = X.iloc[i].values
+            y_i = y[i]
+            pred = x_i.dot(theta.T)
+            for j in range(7):
+                theta[j] = theta[j] - (1 / m) * r *(pred - y_i) * x_i[j]
+            cost += cost_function(x_i,y_i,theta)
+        theta_arr[iter ] = theta
         cost_arr[iter] = cost
-
-
-
-
-
-    return w[iterations -1], cost_arr
-
-
-
-
-    """
-    m = len(X)
-    cost_arr = np.zeros(iterations)
-
-    y = X.output
-    X = X[['Cement', 'Slag', 'Fly ash', 'Water', 'SP', 'Coarse Aggr', 'Fine Aggr']]
-    for iter in range(iterations):
-        cost = 0
-        for i in range(m):
-            for t in range(7):
-
-                random_sample = np.random.randint(0,m)
-                x_i = X.iloc[random_sample].values
-                y_i = y[random_sample]
-                pred = x_i.dot(theta)
-
-
-                theta = theta - (1 / m) * r * (x_i.T.dot((pred - y_i)))
-                cost += cost_function(x_i,y_i,theta)
-            cost_arr[iter] = cost
+        #if iter > 0:
+            #if cost_arr[iter]  < (1 / np.power(10, 6)):
+                #converged = True
+                #return converged, theta, cost_arr
 
     return theta,cost_arr
 
-
-def test_cost_function(X,y,theta):
-    arr = []
-    for i in range(len(X)):
-        arr.append((y[i] - np.dot(theta.T, X[i]))**2)
-    return (1/2)*np.sum(arr)
 
 if __name__ == '__main__':
 
@@ -161,8 +128,30 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
     """
-    theta_B= np.zeros(7)
-    theta_SGD,cost_SGD = stochastic_gradient_descent(df,theta_B, .0025,1000)
-    plt.plot(range(1000),cost_SGD ,color='red', label= 0.25)
+    """
+    converged_sto = False
+    r_sto = 1
+    iter_sto = 10000
+
+    # GOAL HERE IS TO FIND CORRECT R VALUE WITH THE FINAL DESIRED WEIGHT VECTOR
+    r_did_converge_sto = False
+    final_r_sto = 0
+    final_cost_history_sto = []
+    final_weight_vector_sto = []
+    while r_did_converge_sto == False:
+        theta_B = np.zeros(7)
+        did_converge_sto,theta_SGD, cost_SGD = stochastic_gradient_descent(df, theta_B,r_sto, iter_sto)
+
+        if did_converge_sto == True:
+            r_did_converge_sto = did_converge_sto
+            final_r_sto = r_sto
+            final_cost_history_sto = cost_SGD
+            final_weight_vector_sto = theta_SGD
+        else:
+            r_sto = r_sto / 2
+    """
+    theta_B = np.zeros(7)
+    theta_SGD, cost_SGD = stochastic_gradient_descent(df, theta_B, 0.005, 5000)
+    plt.plot(range(5000), cost_SGD, color='red', label=r)
     plt.show()
 
